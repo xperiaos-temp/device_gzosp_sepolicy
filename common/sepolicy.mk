c1ee58cb628186198765a7684fa587641e78f2a3
Paul Keith <javelinanddart@gmail.com>
2018-02-24 21:39:42 +0100

    gzosp_sepolicy: Add sf_lcd_density_prop type and labelled props
    * Fixes sepolicy for recovery for devices that don't
      include device/qcom/sepolicy (like Nexus/Pixel)
    
    Change-Id: Iae1e15b524514ab30634b5e896603c43616faebc

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,6 +3,9 @@
 # inherit from Lineage
 #
 
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/gzosp/sepolicy/common/public
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/gzosp/sepolicy/common/private
 
