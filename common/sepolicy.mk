Atomic-OS/device_aos_sepolicy

sepolicy: use correct flag for PRIVATE_SEPOLICY_DIR
Change-Id: I3c8ab7cb30902c3d64d1b180ed4c75e1cbb5681c
Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

common/sepolicy.mk
@@ -4,5 +4,7 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/aos/sepolicy/common/vendor \
+    device/aos/sepolicy/common/vendor
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aos/sepolicy/common/private

xperiaos-temp/device_gzosp_sepolicy

common/sepolicy.mk
@@ -3,6 +3,8 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/gzosp/sepolicy/common/vendor \
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/gzosp/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/gzosp/sepolicy/common/vendor
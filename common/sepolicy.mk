41d143269c4b5107a6d0b81176c4f54e3feecd17
martinusbe <martinusbe@gmail.com>
2018-03-11 11:51:48 +0100

    sepolicy: use correct flag for PRIVATE_SEPOLICY_DIR
    Change-Id: I3c8ab7cb30902c3d64d1b180ed4c75e1cbb5681c

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,6 +3,8 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/gzosp/sepolicy/common/vendor \
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/gzosp/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/gzosp/sepolicy/common/vendor
